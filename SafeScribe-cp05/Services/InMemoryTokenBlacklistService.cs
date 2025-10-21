92a5b8a09f609e553d4fd8c0e6b6c99af42161e2
Lice-Santos <alicenuunes05@gmail.com>
2025-10-21 16:18:42 -0300

    Código organizado

--- SafeScribe-cp05/Services/InMemoryTokenBlacklistService.cs
+++ SafeScribe-cp05/Services/InMemoryTokenBlacklistService.cs
@@ -4,13 +4,11 @@ using System.Threading.Tasks;
 
 namespace SafeScribe_cp05.Services
 {
-    // A implementação usa memória (HashSet) para armazenar os JTIs revogados.
+    //FUNÇÃO PARA A BLACKLIST FUNCIONAR
     public class InMemoryTokenBlacklistService : ITokenBlacklistService
     {
-        // HashSet é ideal para pesquisa rápida (O(1))
         private static readonly HashSet<string> _blacklistedJtis = new HashSet<string>();
 
-        // Lock para garantir que o acesso ao HashSet seja thread-safe
         private static readonly object _lock = new object();
 
         public Task AddToBlacklistAsync(string jti)
